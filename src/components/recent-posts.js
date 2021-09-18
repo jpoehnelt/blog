e8a9da8d87a5a331e9673ed3eca230df7f86b380
Justin Poehnelt <jpoehnelt@google.com>
2021-09-18 00:55:48 -0600

    fix: recent posts

--- src/components/recent-posts.js
+++ src/components/recent-posts.js
@@ -11,19 +11,19 @@ const RecentPosts = ({posts}) => {
           const title = post.frontmatter.title || post.fields.slug;
 
           return (
-            <li key={post.fields.slug}>
+            <li key={post.fields.slug} className={""}>
               <article
                 className="post-list-item"
                 itemScope
                 itemType="http://schema.org/Article"
               >
                 <header>
-                  <h4>
+                  <h3 className={"mb-2"}>
                     <Link to={post.fields.slug} itemProp="url">
                       <span itemProp="headline">{title}</span>
                     </Link>
-                  </h4>
-                  <small>{post.frontmatter.date}</small>
+                  </h3>
+                  <div>{post.frontmatter.date}</div>
                 </header>
                 <section>
                   <p
