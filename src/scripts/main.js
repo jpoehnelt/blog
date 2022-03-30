76e73c1bc0838cded5b0809ced5540ad450e2fdb
Justin Poehnelt <justin.poehnelt@gmail.com>
2022-03-30 01:48:38 -0600

    feat: tracking

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,54 @@
 import "../styles/tailwind.css";
-import { handleCodeCopying } from "./copy.js";
+
 
 window.addEventListener("load", () => {
-  handleCodeCopying();
+  let cid;
+
+  if (document.cookie) {
+    cid = document.cookie.split(";")[0].split("=").pop()
+  }
+
+  if (!cid) {
+    cid = crypto.randomUUID().slice(1, 4);
+    document.cookie = `cid=${cid}; path=/`;
+  }
+
+  const rand = () => crypto.randomUUID().slice(1, 5);
+  const dr = document.referrer;
+  const dl = window.location.href;
+  const dt = document.title;
+
+  const el = document.getElementById('secure');
+  const img = document.createElement('img')
+  img.classList.add('h-4', 'w-4', 'inline');
+
+  const send = (params) => {
+    params = {
+      cid,
+      dr,
+      dl,
+      dt,
+      z: rand(),
+      ...params
+    }
+    const ENC = {
+      '+': '-',
+      '/': '_',
+      '=': '.'
+    }
+
+    const qs = btoa(JSON.stringify(params)).replace(/[+/=]/g, (m) => ENC[m])
+
+    img.src = `/secure.svg?q=${qs}`
+
+    if (el.childElementCount === 0) {
+      el.appendChild(img);
+    }
+  }
+
+  send({ t: 'pageview' });
+
+  window.onerror = function (message) {
+    send({ t: 'exception', ni: '1', exd: message });
+  }
 });
