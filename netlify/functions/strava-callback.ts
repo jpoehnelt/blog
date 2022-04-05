2b2b9e1856fbf206150ef30dd3767ef9328cf036
Justin Poehnelt <justin.poehnelt@gmail.com>
2022-04-04 23:45:08 -0600

    fix: strava env vars

--- netlify/functions/strava-callback.ts
+++ netlify/functions/strava-callback.ts
@@ -19,7 +19,7 @@ export const handler: Handler = async (event): Promise<HandlerResponse> => {
 
 
     let response: AxiosResponse<Connection>;
-    const url = `https://www.strava.com/oauth/token?client_id=${process.env.STRAVA_OAUTH_CLIENT_ID}&client_secret=${process.env.STRAVA_OAUTH_CLIENT_SECRET}&code=${code}&grant_type=authorization_code`;
+    const url = `https://www.strava.com/oauth/token?client_id=${process.env.STRAVA_CLIENT_ID}&client_secret=${process.env.STRAVA_CLIENT_SECRET}&code=${code}&grant_type=authorization_code`;
 
     try {
         response = await axios.post(url, {});
