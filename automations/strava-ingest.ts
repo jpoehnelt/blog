9f2355de2086c16a72303c56adf1d388cdf52c95
Justin Poehnelt <justin.poehnelt@gmail.com>
2024-01-28 16:55:01 -0800

    build: improve ingest of strava activities (#363)

--- automations/strava-ingest.ts
+++ automations/strava-ingest.ts
@@ -6,7 +6,8 @@ import fs from 'fs';
 
 const main = async () => {
 
-    const after = getUnixTime(process.env.STRAVA_AFTER ? parseISO(process.env.STRAVA_AFTER) : subDays(endOfToday(), 10)) / 1000;
+    const before = getUnixTime(process.env.STRAVA_BEFORE ? parseISO(process.env.STRAVA_BEFORE) : endOfToday());
+    const after = getUnixTime(process.env.STRAVA_AFTER ? parseISO(process.env.STRAVA_AFTER) : subDays(endOfToday(), 5));
     const file = path.join('src', '_data', 'strava.json');
 
     let page = 1;
@@ -16,8 +17,9 @@ const main = async () => {
     // eslint-disable-next-line
     while (true) {
         const per_page = 100;
+        const url = `https://www.strava.com/api/v3/athlete/activities?page=${page}&per_page=${per_page}&before=${before}&after=${after}`;
 
-        const url = `https://www.strava.com/api/v3/athlete/activities?page=${page}&per_page=${per_page}`;//&after=${after}`;
+        console.log({ url })
 
         // eslint-disable-next-line
         const data: any[] = (
@@ -42,8 +44,19 @@ const main = async () => {
     const existing = JSON.parse(fs.readFileSync(file, 'utf8'));
     const updated = { ...existing, ...activities };
 
-    fs.writeFileSync(file, JSON.stringify(updated, null, 2))
+    fs.writeFileSync(file, JSON.stringify(updated, null, 2));
 
+    for (const id of Object.keys(activities)) {
+        const activity = (await axios.get(`https://www.strava.com/api/v3/activities/${id}`, {
+            headers: {
+                Authorization: `Bearer ${process.env.STRAVA_ACCESS_TOKEN}`,
+            },
+        })).data;
+
+        const fileName = `${id}.json`;
+        const filePath = path.join('src', '_data', 'strava', fileName); 
+        fs.writeFileSync(filePath, JSON.stringify(activity, null, 2));
+    }
 }
 
 await main();
