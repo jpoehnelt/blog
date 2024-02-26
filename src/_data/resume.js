c243478d8ef2573e456ca8ce9303ea38b3503a06
Justin Poehnelt <justin.poehnelt@gmail.com>
2024-02-25 22:57:22 -0700

    fix: impacts instead of responsibilities

--- src/_data/resume.js
+++ src/_data/resume.js
@@ -101,37 +101,37 @@ const experiences = [
     endDate: "Present",
     highlights: toHighlights([
       {
-        text: "Led Angular web app development to create an experience interactively exploring and debugging Google Workspace APIs.",
+        text: "Created an interactive Angular web app that allows users to explore and debug Google Workspace APIs, resulting in an enhanced developer experience.",
         types: [Focus.DEVREL, Focus.FULLSTACK, Focus.FRONTEND],
         score: 1,
       },
       {
-        text: "Crafted impactful demos and blog content for high-profile product launches.",
+        text: "Crafted impactful demos and blog content for high-profile product launches, contributing to successful product adoption and customer engagement.",
         types: [Focus.DEVREL],
         score: 0.7,
       },
       {
-        text: "Presented and engaged with customers at conferences like Google I/O, Google Next, and Workspace Developer Summits, demonstrating technical prowess.",
+        text: "Presented and engaged with customers at conferences like Google I/O, Google Next, and Workspace Developer Summits, showcasing technical prowess and building strong relationships.",
         types: [Focus.DEVREL],
         score: 1,
       },
       {
-        text: "Established industry-best practices for Google Maps JavaScript samples, leveraging TypeScript, modern JavaScript, and popular ecosystem tools.",
+        text: "Established industry-best practices for Google Maps JavaScript samples, leveraging TypeScript, modern JavaScript, and popular ecosystem tools, resulting in improved code quality and developer productivity.",
         types: [Focus.DEVREL, Focus.FRONTEND, Focus.FULLSTACK, Focus.OPS],
         score: 0.5,
       },
       {
-        text: "Enhanced Google Maps JavaScript API with Promise support while seamlessly maintaining the existing callback pattern.",
+        text: "Enhanced the Google Maps JavaScript API by adding Promise support while seamlessly maintaining the existing callback pattern, simplifying asynchronous programming for developers.",
         types: [Focus.DEVREL, Focus.FRONTEND],
         score: 0.9,
       },
       {
-        text: "Published impactful NPM libraries like [@googlemaps/js-api-loader](https://www.npmjs.com/package/@googlemaps/js-api-loader) to seamlessly integrate Google Maps JavaScript API with modern frameworks.",
+        text: "Published impactful NPM libraries like [@googlemaps/js-api-loader](https://www.npmjs.com/package/@googlemaps/js-api-loader) to seamlessly integrate Google Maps JavaScript API with modern frameworks, enabling developers to easily incorporate maps into their applications.",
         types: [Focus.DEVREL, Focus.FRONTEND, Focus.FULLSTACK, Focus.OPS],
         score: 1,
       },
       {
-        text: "Generated OpenAPI specs for Google Maps APIs, exported to Postman collections, and created interactive documentation using Bazel and markdown.",
+        text: "Generated OpenAPI specs for Google Maps APIs, exported them to Postman collections, and created interactive documentation using Bazel and markdown, improving API discoverability and developer onboarding.",
         types: [
           Focus.DEVREL,
           Focus.FRONTEND,
@@ -143,22 +143,22 @@ const experiences = [
         score: 1,
       },
       {
-        text: "Automated TypeScript typings generation for Google Maps APIs, robustly pushed to the DefinitelyTyped repository and [@types/google.maps](https://www.npmjs.com/package/@types/google.maps).",
+        text: "Automated TypeScript typings generation for Google Maps APIs, ensuring accurate and up-to-date typings for developers. Contributed to the DefinitelyTyped repository and [@types/google.maps](https://www.npmjs.com/package/@types/google.maps), improving the developer experience.",
         types: [Focus.DEVREL, Focus.FRONTEND, Focus.FULLSTACK, Focus.OPS],
         score: 1,
       },
       {
-        text: "Created an [ESLint plugin for Google Maps JavaScript](https://github.com/googlemaps/eslint-plugin-googlemaps) to enhance the developer experience.",
+        text: "Improved developer experience by creating an [ESLint plugin for Google Maps JavaScript](https://github.com/googlemaps/eslint-plugin-googlemaps).",
         types: [Focus.DEVREL, Focus.FRONTEND, Focus.FULLSTACK, Focus.OPS],
         score: 0.7,
       },
       {
-        text: "Automated testing and releases for [github.com/googlemaps](https://github.com/googlemaps), implementing policies for an engaged developer community. Published key NPM packages and GitHub Apps, including [semantic-release-replace-plugin](https://www.npmjs.com/package/@google/semantic-release-replace-plugin), [secrets-sync-action](https://github.com/google/secrets-sync-action), [semantic-release-interval](https://github.com/jpoehnelt/semantic-release-interval), [semantic-release-npm-deprecate](https://github.com/jpoehnelt/semantic-release-npm-deprecate), and an [inclusive language GitHub bot](https://github.com/jpoehnelt/in-solidarity-bot).",
+        text: "Streamlined testing and releases for [github.com/googlemaps](https://github.com/googlemaps) by implementing policies for an engaged developer community. Published key NPM packages and GitHub Apps, including [semantic-release-replace-plugin](https://www.npmjs.com/package/@google/semantic-release-replace-plugin), [secrets-sync-action](https://github.com/google/secrets-sync-action), [semantic-release-interval](https://github.com/jpoehnelt/semantic-release-interval), [semantic-release-npm-deprecate](https://github.com/jpoehnelt/semantic-release-npm-deprecate), and an [inclusive language GitHub bot](https://github.com/jpoehnelt/in-solidarity-bot).",
         types: [Focus.DEVREL, Focus.OPS, Focus.FRONTEND],
         score: 0.6,
       },
       {
-        text: "Pioneered feedback processing pipelines with Google Cloud Platform, LLMs, vector embeddings, TF-IDF, and machine learning models. Developed a Chrome extension for team and organization efficiency.",
+        text: "Implemented feedback processing pipelines with Google Cloud Platform, LLMs, vector embeddings, TF-IDF, and machine learning models, resulting in improved team and organization efficiency. Developed a Chrome extension to enhance productivity.",
         types: [
           Focus.DEVREL,
           Focus.OPS,
@@ -170,7 +170,7 @@ const experiences = [
         score: 1,
       },
       {
-        text: "Generated, maintained, and published gRPC client libraries using Bazel and cutting-edge open source tools.",
+        text: "Generated, maintained, and published gRPC client libraries using Bazel and cutting-edge open source tools, enabling seamless integration with other systems.",
         types: [
           Focus.DEVREL,
           Focus.OPS,
@@ -199,7 +199,7 @@ const experiences = [
           Focus.FRONTEND,
           Focus.FULLSTACK,
         ],
-        text: "Designed and implemented a sophisticated satellite imagery analysis abstraction layer, integrating GDAL, NumPy, SciPy, Tensorflow, and Apache Arrow with cloud computing primitives. Published a Python package and extended the DSL and operations graph to enable interactive exploration.",
+        text: "Achieved significant improvements in satellite imagery analysis by designing and implementing a sophisticated abstraction layer, integrating GDAL, NumPy, SciPy, Tensorflow, and Apache Arrow with cloud computing primitives. Published a Python package and extended the DSL and operations graph to enable interactive exploration.",
         score: 1,
       },
       {
@@ -210,7 +210,7 @@ const experiences = [
           Focus.FULLSTACK,
           Focus.OPS,
         ],
-        text: "Led a team in developing a robust distributed system for processing petabytes of satellite imagery, utilizing Python, Golang, Docker, Kubernetes, and Google Cloud Platform.",
+        text: "Led a team in developing a robust distributed system for processing petabytes of satellite imagery, utilizing Python, Golang, Docker, Kubernetes, and Google Cloud Platform. Resulted in significant performance improvements and increased scalability.",
         score: 1,
       },
       {
@@ -222,7 +222,7 @@ const experiences = [
           Focus.FULLSTACK,
           Focus.OPS,
         ],
-        text: "Successfully migrated the company codebase to a monorepo using Bazel, implementing a state-of-the-art distributed build and deployment system with tools such as Spinnaker, Drone, Helm, Jsonnet, and Kubernetes.",
+        text: "Successfully migrated the company codebase to a monorepo using Bazel, implementing a state-of-the-art distributed build and deployment system with tools such as Spinnaker, Drone, Helm, Jsonnet, and Kubernetes. Streamlined development processes and improved collaboration across teams.",
         score: 0.9,
       },
       {
@@ -233,7 +233,7 @@ const experiences = [
           Focus.FULLSTACK,
           Focus.OPS,
         ],
-        text: "Implemented an OpenResty API gateway and eventually migrated to Envoy proxy, establishing a streamlined service mesh with Istio.",
+        text: "Implemented an OpenResty API gateway and eventually migrated to Envoy proxy, establishing a streamlined service mesh with Istio. Improved service reliability and performance.",
         score: 0.7,
       },
       {
@@ -243,7 +243,7 @@ const experiences = [
           Focus.FRONTEND,
           Focus.FULLSTACK,
         ],
-        text: "Designed and implemented an advanced GeoSpatial Vector service on BigQuery using Go and gRPC.",
+        text: "Designed and implemented an advanced GeoSpatial Vector service on BigQuery using Go and gRPC. Enabled efficient and scalable geospatial data processing.",
         score: 0.6,
       },
       {
@@ -255,7 +255,7 @@ const experiences = [
           Focus.FULLSTACK,
           Focus.OPS,
         ],
-        text: "Implemented rigorous software engineering protocols, including thorough code reviews, comprehensive testing, and documentation. Led impactful cross-functional team guild dialogues to foster a culture of knowledge sharing and collaboration.",
+        text: "Implemented rigorous software engineering protocols, including thorough code reviews, comprehensive testing, and documentation. Led impactful cross-functional team guild dialogues to foster a culture of knowledge sharing and collaboration. Resulted in improved code quality and increased productivity.",
         score: 0.5,
       },
       {
@@ -266,7 +266,7 @@ const experiences = [
           Focus.FULLSTACK,
           Focus.OPS,
         ],
-        text: "Engineered an approximate nearest neighbor search using locality-sensitive hashing and vector embeddings, seamlessly deployed to production with Kubernetes, Google Cloud Platform, Redis, and Google Cloud Storage for multi-layered caching.",
+        text: "Engineered an approximate nearest neighbor search using locality-sensitive hashing and vector embeddings, seamlessly deployed to production with Kubernetes, Google Cloud Platform, Redis, and Google Cloud Storage for multi-layered caching. Improved search performance and reduced latency.",
         score: 0.8,
       },
     ]),
@@ -281,32 +281,32 @@ const experiences = [
     endDate: "July 2016",
     highlights: toHighlights([
       {
-        text: "Developed a Python Flask API on Postgresql with OpenID for authorization.",
+        text: "Developed a Python Flask API on Postgresql with OpenID for authorization, resulting in improved security and user authentication.",
         types: [Focus.BACKEND, Focus.FRONTEND, Focus.FULLSTACK],
         score: 1,
       },
       {
-        text: "Managed asynchronous tasks with Celery and Redis on AWS EC2 instances.",
+        text: "Managed asynchronous tasks with Celery and Redis on AWS EC2 instances, optimizing performance and scalability of distributed systems.",
         types: [Focus.DISTRIBUTED, Focus.BACKEND, Focus.FULLSTACK],
         score: 1,
       },
       {
-        text: "Built a crowd-sourcing web app with Angular, utilizing D3, Leaflet, and custom SVG Angular components for data visualization.",
+        text: "Built a crowd-sourcing web app with Angular, utilizing D3, Leaflet, and custom SVG Angular components for data visualization, enhancing user experience and engagement.",
         types: [Focus.FULLSTACK, Focus.FRONTEND],
         score: 1,
       },
       {
-        text: "Implemented algorithms in Google Earth Engine for real-time spatial analysis, integrating with GDAL, OGC standards, and ArcGIS/ENVI.",
+        text: "Implemented algorithms in Google Earth Engine for real-time spatial analysis, integrating with GDAL, OGC standards, and ArcGIS/ENVI, enabling efficient and accurate geospatial data processing.",
         types: [Focus.BACKEND, Focus.FULLSTACK, Focus.FRONTEND],
         score: 0.4,
       },
       {
-        text: "Designed, implemented, and published a mobile app for ground data collection in sub-Saharan Africa.",
+        text: "Designed, implemented, and published a mobile app for ground data collection in sub-Saharan Africa, facilitating data collection and analysis for research purposes.",
         types: [Focus.FULLSTACK, Focus.FRONTEND],
         score: 0.5,
       },
       {
-        text: "Established version control practices, migrating algorithms to Git, and versioned model inputs in cloud storage.",
+        text: "Established version control practices, migrating algorithms to Git, and versioned model inputs in cloud storage, ensuring proper tracking and management of code and data.",
         types: [Focus.OPS],
         score: 0.3,
       },
@@ -330,7 +330,7 @@ const experiences = [
           Focus.FULLSTACK,
           Focus.OPS,
         ],
-        text: "Optimized Walmart's IT incident data in a high-performance OLAP system, creating a web app for visualization using Angular and D3. Applied machine learning and statistical analysis for outage prediction.",
+        text: "Led the optimization of Walmart's IT incident data in a high-performance OLAP system, resulting in improved data processing and analysis capabilities. Developed a cutting-edge web application for data visualization using Angular and D3, providing actionable insights for outage prediction through the application of advanced machine learning and statistical analysis techniques.",
         score: 1,
       },
       {
@@ -341,7 +341,7 @@ const experiences = [
           Focus.FULLSTACK,
           Focus.OPS,
         ],
-        text: "Developed a mobile app for user interaction with incident data and real-time outage notifications.",
+        text: "Designed and developed a mobile app for seamless user interaction with incident data and instant real-time outage notifications.",
         score: 0.6,
       },
       {
@@ -353,7 +353,7 @@ const experiences = [
           Focus.OPS,
           Focus.DEVREL,
         ],
-        text: "Contributed to a dynamic team of interns in IBM's Extreme Blue Technical Leader Program.",
+        text: "Collaborated with a highly skilled and innovative team of interns in IBM's prestigious Extreme Blue Technical Leader Program, driving impactful contributions to cutting-edge projects.",
         score: 0,
       },
     ]),
@@ -461,6 +461,7 @@ const skills = [
       "Netlify",
       "Nginx",
       "OpenTelemetry",
+      "OpenAI",
       "Postgresql",
       "Proxmox",
       "Redis",
@@ -468,6 +469,7 @@ const skills = [
       "Spinnaker",
       "Terraform",
       "Vercel",
+      "VertexAI",
       "Vultr",
     ],
   },
