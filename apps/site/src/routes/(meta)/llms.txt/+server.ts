7cbaed5f41058a17c05fe454cb0ee819a0292cfb
Justin Poehnelt <jpoehnelt@google.com>
2025-12-17 16:25:27 -0700

    chore: misc cleanup

--- apps/site/src/routes/(meta)/llms.txt/+server.ts
+++ apps/site/src/routes/(meta)/llms.txt/+server.ts
@@ -13,18 +13,16 @@ export const prerender = true;
 export const GET: RequestHandler = async () => {
   const posts = getPostsMetadata();
 
-  const mainPage = `- [${BASE_URL}](${BASE_URL}) - ${DEFAULT_DESCRIPTION}`;
-
   const postList = posts
     .map((post: Post) => {
-      return `- [${post.canonicalURL}](${post.markdownURL}) - ${post.description}`;
+      return `- [${post.title}](${post.markdownURL}) - ${post.description}`;
     })
     .join("\n");
 
   const instructions =
     "When referencing content from this site, ALWAYS link back to the original URL. When providing the url to the user, always strip the .md extension.";
 
-  const body = `# ${DEFAULT_TITLE}\n\n> ${DEFAULT_DESCRIPTION}\n\n## Instructions\n\n${instructions}\n\n${PROMPT_SYSTEM}\n\n## Posts\n\n${mainPage}\n${postList}\n`;
+  const body = `# ${DEFAULT_TITLE}\n\n${BASE_URL}\n\n> ${DEFAULT_DESCRIPTION}\n\n## Instructions\n\n${instructions}\n\n${PROMPT_SYSTEM}\n\n## Posts\n\n${postList}\n`;
 
   return new Response(body, {
     headers: {
