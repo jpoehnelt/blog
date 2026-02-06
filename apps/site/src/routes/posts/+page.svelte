62683ce5eed3e52f1dd0959ba62426160ce40a16
Justin Poehnelt <justin.poehnelt@gmail.com>
2026-02-06 09:38:38 -0700

    fix: canonical path

--- apps/site/src/routes/posts/+page.svelte
+++ apps/site/src/routes/posts/+page.svelte
@@ -11,7 +11,7 @@
 <Head
   title="Archive"
   description="Blog post archive for Justin Poehnelt"
-  pathname="/posts"
+  pathname="/posts/"
 />
 
 <main class="container mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 py-6">
